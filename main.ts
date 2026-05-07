09200b21023f1481ece001194354aa2c4a3147f3
Aaryan <mineyjava@gmail.com>
2026-05-07 09:39:23 +0930

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,31 @@
 function Jiryeotdari () {
-    music.play(music.stringPlayable("F - A A - C5 C5 - ", 1000), music.PlaybackMode.InBackground)
-    basic.showIcon(IconNames.Happy)
+    if (Steps == Goal) {
+        if (!(Goal == 0)) {
+            music.play(music.stringPlayable("F - A A - C5 C5 - ", 1000), music.PlaybackMode.InBackground)
+            basic.showIcon(IconNames.Happy)
+        }
+    }
 }
 function Goals (Goal_Number: number, Goal_Reached: boolean) {
-    Is_Setting_Goal = true
-    music.play(music.stringPlayable("F G - B - - - - ", 500), music.PlaybackMode.InBackground)
-    basic.showIcon(IconNames.Target)
-    basic.showNumber(Goal)
+    if (!(Is_Setting_Goal)) {
+        Is_Setting_Goal = true
+        music.play(music.stringPlayable("F G - B - - - - ", 500), music.PlaybackMode.InBackground)
+        basic.showIcon(IconNames.Target)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showNumber(Goal)
+        basic.showIcon(IconNames.Target)
+    }
 }
 input.onButtonPressed(Button.A, function () {
-    if (!(Is_Setting_Goal)) {
-        Goals(1, true)
-    }
+    Goals(1, true)
 })
+// There's no block equivalent for this. Just makes it scroll faster
 function Scrolls_Goals () {
     if (Is_Setting_Goal && input.buttonIsPressed(Button.B)) {
         Goal_Index += 1
@@ -28,7 +41,13 @@ function Scrolls_Goals () {
             . . . . .
             `)
         basic.showNumber(Goal, 35)
-basic.showIcon(IconNames.Target)
+// There's no block equivalent for this. Just makes it scroll faster
+        basic.showIcon(IconNames.Target)
+    }
+}
+function Time_Reset () {
+    if (Time == 5) {
+        Reset()
     }
 }
 function Stepping () {
@@ -51,23 +70,26 @@ function Physics_Stuff () {
 function Reset () {
     Chill = 250
     Steps = 0
-    Min_Force = 1750
+    Min_Force = 2500
     Force = 0
     Time = 0
     Goal_Index = 0
     Goal = 0
     Is_Setting_Goal = false
 }
-let Time = 0
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Is_Setting_Goal = false
+})
 let z = 0
 let y = 0
 let x = 0
-let Steps = 0
 let Chill = 0
 let Min_Force = 0
 let Force = 0
+let Time = 0
 let Goal_Index = 0
 let Is_Setting_Goal = false
+let Steps = 0
 let Goal_List: number[] = []
 let Goal = 0
 Goal_List = [
@@ -85,15 +107,15 @@ Goal_List = [
 ]
 Reset()
 basic.forever(function () {
-    if (!(Is_Setting_Goal)) {
-        basic.showNumber(Steps)
+    Scrolls_Goals()
+    if (Is_Setting_Goal && input.buttonIsPressed(Button.A)) {
+        music.play(music.stringPlayable("C5 C5 - A A - E - ", 420), music.PlaybackMode.InBackground)
+        basic.pause(100)
     }
 })
 basic.forever(function () {
-    Scrolls_Goals()
-    if (Is_Setting_Goal && input.buttonIsPressed(Button.A)) {
-        music.play(music.stringPlayable("C5 C5 - A A - E - ", 120), music.PlaybackMode.InBackground)
-        Is_Setting_Goal = false
+    if (!(Is_Setting_Goal)) {
+        basic.showNumber(Steps)
     }
 })
 control.inBackground(function () {
@@ -102,5 +124,6 @@ control.inBackground(function () {
         Physics_Stuff()
         Time = input.runningTime() / 1000
         Stepping()
+        Time_Reset()
     }
 })
